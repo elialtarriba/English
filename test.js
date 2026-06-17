ca2396f404f9c8cb88b19166475edd931f41fdba
Elisabeth <elialtarriba@gmail.com>
2026-06-17 08:18:59 +0200

    cambios esenciales

--- test.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('/Users/elisabeth/Library/Mobile Documents/com~apple~CloudDocs/ELI_ENGLISH/English.html', 'utf8');
-const match = html.match(/<script>([\s\S]*?)<\/script>/g);
-if (match) {
-  match.forEach((script, i) => {
-    fs.writeFileSync(`script_${i}.js`, script.replace(/<\/?script>/g, ''));
-  });
-}
