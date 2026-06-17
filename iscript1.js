8af7226184433a226ca77ff689570619346dbb9d
Elisabeth <elialtarriba@gmail.com>
2026-06-17 18:26:35 +0200

    iconos

--- iscript1.js
+++
@@ -1,4 +0,0 @@
-
-    function requestFlashcards(){openPasswordDialog('flashcards',()=>window.parent.postMessage({action:'openVerbStudyEditor'}, '*'));}
-    function requestQuizEditor(){openPasswordDialog('quiz',()=>window.parent.postMessage({action:'openVerbQuizEditor'}, '*'));}
-    
